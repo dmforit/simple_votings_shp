be56b354b0d79b3ea245ea102a6c83f464d09371
BuildTools <unconfigured@null.spigotmc.org>
2021-01-02 22:40:13 +0400

    unique id

--- vote/models.py
+++ vote/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 from django.utils import timezone
 
 class Vote(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     options = models.CharField(max_length=500)
     votes = models.CharField(max_length=500)
