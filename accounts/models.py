2a924f81f160d91dbb343c763f58d3e7b0e644a6
shar3nda <vyavya2003@yandex.ru>
2021-01-06 16:29:27 +0200

    name avatar files randomly

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,21 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+import os
+from uuid import uuid4
 
-# TODO name avatar files uniquely
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# TODO delete file on delete or change avatar
+def path_and_rename(path, old_avatar=None):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        filename = '{}.{}'.format(uuid4().hex, ext)
+        return os.path.join(path, filename)
+    return wrapper
 
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(null=True, blank=True, upload_to='images/avatars')
+    avatar = models.ImageField(null=True, blank=True, upload_to=path_and_rename('images/avatars'))
 
     def __str__(self):
         return self.username
