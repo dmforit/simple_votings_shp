c2168375af4a413c64fd814fa69548300562412b
shar3nda <vyavya2003@yandex.ru>
2021-01-31 17:57:18 +0200

    remove from favorites

--- favorites/views.py
+++ favorites/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from vote.models import Vote
+from accounts.models import CustomUser
 
 # Create your views here.
 
+
 def favorites(request):
-    ctx = Vote.objects.all
+    ctx = request.user.fav_votes.all
 
     return render(request, "favorites/favorites.html", {"all": ctx})
+
+
+def fav_view_remove(request, pk):
+    vote = get_object_or_404(Vote, id=request.POST.get('vote_id'))
+    request.user.fav_votes.remove(vote.id)
+    return HttpResponseRedirect(reverse('favorites'))
